99117121b9d13ec0a762a3ae701f461f6e07b45f
MR-ATX\johnw <johnwhitakeraustin@gmail.com>
2022-09-26 00:51:02 -0500

    Adding poster pics

--- static/js/np_testerz.js
+++ static/js/np_testerz.js
@@ -1,15 +1,69 @@
-function testerz(data){
-    console.log("Johnny fuckin' pwns!!!")
-    console.log(data.name)
-    console.log(data.role)
+// function testerz(data){
+//     console.log("Johnny fuckin' pwns!!!")
+//     console.log(data.name)
+//     console.log(data.role)
     
+// }
+
+// d3.json('https://mratx.github.io/sample.json').then(data => 
+//     testerz(data),
+//     );
+
+
+
+// function picloop(pics){
+//     for(i in pics){
+//         console.log(i)
+//     }
+// }
+
+// // $.getJSON('https://mratx.github.io/sample.json', function(data) {
+// //     console.log(data.name)
+// //     console.log(data.phonenumber)
+// // });
+
+// d3.select("#posterrow")
+// .append("div")
+// .attr("class","container-fluid")
+// .append("div")
+// .attr("class","jumbotron")
+// .append("h1")
+// .attr("class","jumbotitle")
+// .text(gameovertext)
+// .append("hr")
+// render_player_roster(winner,"end")
+
+
+function posterz(data){
+    console.log(data)
+    console.log(data["Big Bend"].img_url)
+    for(i in data){
+        if(i != "parks" || i != "years"){
+            console.log(data[i].img_url)
+            j = "https://mratx.github.io/static/images/posters/" + i.img_url + ".png"
+            // temp
+            l = data[i].img_url
+            //temp
+            d3.select("#posterrow")
+                .append("div")
+                .attr("class","row")
+                .attr("id","postercol")
+                .append("a")
+                .attr("href",j)
+                .append("img")
+                .attr("src",l)
+        }
+
+    }
 }
 
-d3.json('https://mratx.github.io/sample.json').then(data => 
-    testerz(data),
-    );
+d3.json("https://mratx.github.io/park_info.json").then(data=>
+    posterz(data)
+);
 
-// $.getJSON('https://mratx.github.io/sample.json', function(data) {
-//     console.log(data.name)
-//     console.log(data.phonenumber)
-// });
+// <div class="col-md-1" id="postercol">
+// <a href="static/images/posters/acadia.png" target="_blank">
+// <img src="static/images/posters/acadia.png" id="poster" href="static/images/posters/acadia.png" target="_blank">
+// </a>
+// </div>
+// <div class="row" id="posterrow">
